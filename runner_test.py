1ef882e95cc68b777530d59b9859c1d82252cee8
ariyaths <anu.sujit@gmail.com>
2022-06-12 09:55:51 +0530

    Update runner_test.py

--- runner_test.py
+++ runner_test.py
@@ -8,44 +8,44 @@ Created on Sun Jun 12 07:45:50 2022
 from os import mkdir as mk, listdir as ld
 from shutil import rmtree
 
-mk(r"C:\Fuji")
-mk(r"C:\Fuji\23-04-22")
-mk(r"C:\Fuji\23-04-22\jpg")
-mk(r"C:\Fuji\23-04-22\raw")
-mk(r"C:\Fuji\24-04-22")
-mk(r"C:\Fuji\24-04-22\jpg")
-mk(r"C:\Fuji\24-04-22\raw")
-mk(r"C:\Fuji\deleted")
-mk(r"C:\Fuji\Family")
-open(r"C:\Fuji\deleted\A.jpg", "w")
-open(r"C:\Fuji\deleted\B.jpg", "w")
-open(r"C:\Fuji\Family\D.jpg", "w")
-open(r"C:\Fuji\Family\F.jpg", "w")
-open(r"C:\Fuji\23-04-22\jpg\A.jpg", "w")
-open(r"C:\Fuji\23-04-22\jpg\B.jpg", "w")
-open(r"C:\Fuji\23-04-22\jpg\D.jpg", "w")
-open(r"C:\Fuji\23-04-22\jpg\F.jpg", "w")
-open(r"C:\Fuji\23-04-22\raw\A.raw", "w")
-open(r"C:\Fuji\23-04-22\raw\B.raw", "w")
-open(r"C:\Fuji\23-04-22\raw\D.raw", "w")
-open(r"C:\Fuji\23-04-22\raw\E.raw", "w")
-open(r"C:\Fuji\24-04-22\jpg\A.jpg", "w")
-open(r"C:\Fuji\24-04-22\jpg\B.jpg", "w")
-open(r"C:\Fuji\24-04-22\jpg\D.jpg", "w")
-open(r"C:\Fuji\24-04-22\jpg\F.jpg", "w")
-open(r"C:\Fuji\24-04-22\raw\A.raw", "w")
-open(r"C:\Fuji\24-04-22\raw\B.raw", "w")
-open(r"C:\Fuji\24-04-22\raw\D.raw", "w")
-open(r"C:\Fuji\24-04-22\raw\E.raw", "w")
+mk(r"C:\fuji_test_test")
+mk(r"C:\fuji_test\23-04-22")
+mk(r"C:\fuji_test\23-04-22\jpg")
+mk(r"C:\fuji_test\23-04-22\raw")
+mk(r"C:\fuji_test\24-04-22")
+mk(r"C:\fuji_test\24-04-22\jpg")
+mk(r"C:\fuji_test\24-04-22\raw")
+mk(r"C:\fuji_test\deleted")
+mk(r"C:\fuji_test\Family")
+open(r"C:\fuji_test\deleted\A.jpg", "w")
+open(r"C:\fuji_test\deleted\B.jpg", "w")
+open(r"C:\fuji_test\Family\D.jpg", "w")
+open(r"C:\fuji_test\Family\F.jpg", "w")
+open(r"C:\fuji_test\23-04-22\jpg\A.jpg", "w")
+open(r"C:\fuji_test\23-04-22\jpg\B.jpg", "w")
+open(r"C:\fuji_test\23-04-22\jpg\D.jpg", "w")
+open(r"C:\fuji_test\23-04-22\jpg\F.jpg", "w")
+open(r"C:\fuji_test\23-04-22\raw\A.raw", "w")
+open(r"C:\fuji_test\23-04-22\raw\B.raw", "w")
+open(r"C:\fuji_test\23-04-22\raw\D.raw", "w")
+open(r"C:\fuji_test\23-04-22\raw\E.raw", "w")
+open(r"C:\fuji_test\24-04-22\jpg\A.jpg", "w")
+open(r"C:\fuji_test\24-04-22\jpg\B.jpg", "w")
+open(r"C:\fuji_test\24-04-22\jpg\D.jpg", "w")
+open(r"C:\fuji_test\24-04-22\jpg\F.jpg", "w")
+open(r"C:\fuji_test\24-04-22\raw\A.raw", "w")
+open(r"C:\fuji_test\24-04-22\raw\B.raw", "w")
+open(r"C:\fuji_test\24-04-22\raw\D.raw", "w")
+open(r"C:\fuji_test\24-04-22\raw\E.raw", "w")
 
 execfile(r"C:\code\omk\runner.py")
 
-assert ld(r"C:\Fuji\23-04-22\raw") == ["E.raw"]
-assert ld(r"C:\Fuji\24-04-22\raw") == ["E.raw"]
-assert ld(r"C:\Fuji\23-04-22\jpg") == ["A.jpg", "B.jpg", "D.jpg", "F.jpg"]
-assert ld(r"C:\Fuji\24-04-22\jpg") == ["A.jpg", "B.jpg", "D.jpg", "F.jpg"]
-assert ld(r"C:\Fuji\deleted") == ["A.jpg", "B.jpg"]
-assert ld(r"C:\Fuji\Family") == ["D.jpg", "F.jpg"]
+assert ld(r"C:\fuji_test\23-04-22\raw") == ["E.raw"]
+assert ld(r"C:\fuji_test\24-04-22\raw") == ["E.raw"]
+assert ld(r"C:\fuji_test\23-04-22\jpg") == ["A.jpg", "B.jpg", "D.jpg", "F.jpg"]
+assert ld(r"C:\fuji_test\24-04-22\jpg") == ["A.jpg", "B.jpg", "D.jpg", "F.jpg"]
+assert ld(r"C:\fuji_test\deleted") == ["A.jpg", "B.jpg"]
+assert ld(r"C:\fuji_test\Family") == ["D.jpg", "F.jpg"]
 
 print("Code correct")
-rmtree(r"C:\Fuji")
+rmtree(r"C:\fuji_test")
