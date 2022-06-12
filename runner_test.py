4ebb26dfa80e7d12a44522099a006cec8d832310
ariyaths <anu.sujit@gmail.com>
2022-06-12 16:02:45 +0530

    Updates

--- runner_test.py
+++ runner_test.py
@@ -34,12 +34,12 @@ open(r"C:\fuji_test\24-04-22\jpg\B.jpg", "w")
 open(r"C:\fuji_test\24-04-22\jpg\D.jpg", "w")
 open(r"C:\fuji_test\24-04-22\jpg\F.jpg", "w")
 open(r"C:\fuji_test\24-04-22\raw\A.raw", "w")
-open(r"C:\fuji_test\24-04-22\raw\B.raw", "w")
+open(r"C:\fuji_test\24-04-22\raw\B.raw", "w") 
 open(r"C:\fuji_test\24-04-22\raw\D.raw", "w")
 open(r"C:\fuji_test\24-04-22\raw\E.raw", "w")
 
 exec(open(r"C:\code\omk\runner.py").read())
- 
+
 assert ld(r"C:\fuji_test\23-04-22\raw") == ["E.raw"]
 assert ld(r"C:\fuji_test\24-04-22\raw") == ["E.raw"]
 assert ld(r"C:\fuji_test\23-04-22\jpg") == ["A.jpg", "B.jpg", "D.jpg", "F.jpg"]
